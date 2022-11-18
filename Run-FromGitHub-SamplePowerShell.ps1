0954444e2d71d72786ef50f75884ef86a441ed44
VoodooNexus <pbarlet01@gmail.com>
2022-11-18 11:29:16 +0100

    Update Run-FromGitHub-SamplePowerShell.ps1
    Update executed script with AV check

--- Run-FromGitHub-SamplePowerShell.ps1
+++ Run-FromGitHub-SamplePowerShell.ps1
@@ -1,4 +1,4 @@
-# Demo of PowerShell download and run directly from GitHub
+.# Demo of PowerShell download and run directly from GitHub
 # Note, from a security point of view this is a really bad idea, you shouldn't run code
 # before reviewing it carefully and understanding it, and anyone who has access to the 
 # GitHub repo could  change the code at any time 
@@ -13,9 +13,4 @@
 # $ScriptFromGithHub = Invoke-WebRequest https://raw.githubusercontent.com/tomarbuthnot/Run-PowerShell-Directly-From-GitHub/master/Run-FromGitHub-SamplePowerShell.ps1
 # Invoke-Expression $($ScriptFromGithHub.Content)
 
-
-Write-Host " "
-Write-Host "Here are the top 10 CPU consuming processes right now"
-Write-Host " "
-Get-Process | Sort-Object CPU -Descending | Select-Object -First 10
-
+Get-CimInstance -Namespace root/SecurityCenter2 -ClassName AntivirusProduct
